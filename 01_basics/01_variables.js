1ac8d29178dd745b756576706d5077598970975d
vijaykheni07 <104977612+vijaykheni07@users.noreply.github.com>
2023-10-27 09:24:05 +0000

    upadate

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "kheni@google.com"
+var accountPassword = "12345"
+accountCity = "surat"
+let accountState
+
+// accountId = 2 
+
+console.log(accountId);
+
+accountEmail = "kheni01@gmail.com"
+accountPassword = "777777"
+accountCity = "bhavnagar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
